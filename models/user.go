0a90f1d1e64dda5eb7888687f64b669d3518e85e
Ahmad Assaidy <alsaidyxcode@gmail.com>
2024-09-14 15:51:19 +0300

    using validation

--- models/user.go
+++ models/user.go
@@ -6,12 +6,17 @@ type User struct {
 	Id       int64     `json:"id"`
 	Name     string    `json:"name"`
 	Email    string    `json:"email"`
-	Password string    `json:"password"`
+	Password string    `json:"-"`
 	JoinedAt time.Time `json:"joinedAt"`
 }
 
 type UserCreateOrUpdateRequest struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Name     string `json:"name" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8,max=130"`
+}
+
+type UserLoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8,max=130"`
 }
---
+++ utils/validate.go
@@ -0,0 +1,8 @@
+package utils
+
+import (
+	"github.com/go-playground/validator/v10"
+)
+
+var Validate = validator.New()
+
